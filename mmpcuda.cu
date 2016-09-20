b76c80fe8252ca830c5804db28915019fb7ac063
bertini36 <albertopouquiros@gmail.com>
2016-09-20 09:04:20 +0200

    format

--- mmpcuda.cu
+++ mmpcuda.cu
@@ -1,10 +1,10 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /* PARALLEL MATRIX-MATRIX PRODUCT WITH CUDA                                  */
-/* 											                                 */
-/* File:    	 mmpcuda.cu                                                  */
-/* Author: 		 Alberto Pou Quirós (Github: bertini36)					     */	
+/*                                                                           */
+/* File:         mmpcuda.cu                                                  */
+/* Author:       Alberto Pou Quirós (Github: bertini36)                      */ 
 /* Description:  This program performs a matrix product (A * B = C)          */
-/*				 parallelizing the computation with Nvidia CUDA technology   */
+/*               parallelizing the computation with Nvidia CUDA technology   */
 /* Compilation:  nvcc mmpcuda.cu -o mmpcuda                                  */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -19,107 +19,107 @@
 #define err(format, ...) do { fprintf(stderr, format, ##__VA_ARGS__); exit(1); } while (0)
 
 inline void checkCuda(cudaError_t e) {
-	if (e != cudaSuccess) {
-		err("CUDA Error %d: %s\n", e, cudaGetErrorString(e));
-	}
+    if (e != cudaSuccess) {
+        err("CUDA Error %d: %s\n", e, cudaGetErrorString(e));
+    }
 }
 
 __global__ void matrixProduct(double *matrix_a, double *matrix_b, double *matrix_c, int width) {
-	int sum = 0;
-	int row = threadIdx.y + blockDim.y * blockIdx.y;
-	int col = threadIdx.x + blockDim.x * blockIdx.x;
-	if (col < width && row < width) {
-		for (int k=0; k<width; k++) {
-			sum += matrix_a[row * width + k] * matrix_b[k * width + col];
-		}
-		matrix_c[row * width + col] = sum;
-	}
+    int sum = 0;
+    int row = threadIdx.y + blockDim.y * blockIdx.y;
+    int col = threadIdx.x + blockDim.x * blockIdx.x;
+    if (col < width && row < width) {
+        for (int k=0; k<width; k++) {
+            sum += matrix_a[row * width + k] * matrix_b[k * width + col];
+        }
+        matrix_c[row * width + col] = sum;
+    }
 }
 
 void initializeMatrices(double matrix_a[N][N], double matrix_b[N][N]) {
-	srand(time(NULL));
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<N; j++) {
-			matrix_a[i][j] = rand();
-			matrix_b[i][j] = rand();
-		}
-	}
+    srand(time(NULL));
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            matrix_a[i][j] = rand();
+            matrix_b[i][j] = rand();
+        }
+    }
 }
 
 void showResults(double matrix_a[N][N], double matrix_b[N][N], double matrix_c[N][N]) {
-	printf("***** MATRIX A ***** \n");
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<N; j++) {
-			(j % N == N-1) ? printf("%.1f \n", matrix_a[i][j]) : printf("%.1f,", matrix_a[i][j]);
-		}
-	}
-	printf("***** MATRIX B ***** \n");
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<N; j++) {
-			(j % N == N-1) ? printf("%.1f \n", matrix_b[i][j]) : printf("%.1f,", matrix_b[i][j]);
-		}
-	}
-	printf("***** RESULT MATRIX ***** \n");
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<N; j++) {
-			(j % N == N-1) ? printf("%.1f \n", matrix_c[i][j]) : printf("%.1f,", matrix_c[i][j]);
-		}
-	}
+    printf("***** MATRIX A ***** \n");
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            (j % N == N-1) ? printf("%.1f \n", matrix_a[i][j]) : printf("%.1f,", matrix_a[i][j]);
+        }
+    }
+    printf("***** MATRIX B ***** \n");
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            (j % N == N-1) ? printf("%.1f \n", matrix_b[i][j]) : printf("%.1f,", matrix_b[i][j]);
+        }
+    }
+    printf("***** RESULT MATRIX ***** \n");
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            (j % N == N-1) ? printf("%.1f \n", matrix_c[i][j]) : printf("%.1f,", matrix_c[i][j]);
+        }
+    }
 }
 
 int main() {
 
-	double h_a[N][N], h_b[N][N], h_c[N][N];
-	double *d_a, *d_b, *d_c;
+    double h_a[N][N], h_b[N][N], h_c[N][N];
+    double *d_a, *d_b, *d_c;
 
-	initializeMatrices(h_a, h_b);
+    initializeMatrices(h_a, h_b);
 
-	double size = (double) N * N * sizeof(double);
+    double size = (double) N * N * sizeof(double);
 
-	// Allocate memory in the device
-	checkCuda(cudaMalloc((void **) &d_a, size));
-	checkCuda(cudaMalloc((void **) &d_b, size));
-	checkCuda(cudaMalloc((void **) &d_c, size));
+    // Allocate memory in the device
+    checkCuda(cudaMalloc((void **) &d_a, size));
+    checkCuda(cudaMalloc((void **) &d_b, size));
+    checkCuda(cudaMalloc((void **) &d_c, size));
 
-	// Copy the information in the device
-	checkCuda(cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice));
-	checkCuda(cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice));
+    // Copy the information in the device
+    checkCuda(cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice));
+    checkCuda(cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice));
 
-	// CUDA threads structure definition
-	dim3 dimGrid((N + BLOCK_SIZE_DIM -1) / BLOCK_SIZE_DIM, (N + BLOCK_SIZE_DIM -1) / BLOCK_SIZE_DIM);
-	dim3 dimBlock(BLOCK_SIZE_DIM, BLOCK_SIZE_DIM);	
+    // CUDA threads structure definition
+    dim3 dimGrid((N + BLOCK_SIZE_DIM -1) / BLOCK_SIZE_DIM, (N + BLOCK_SIZE_DIM -1) / BLOCK_SIZE_DIM);
+    dim3 dimBlock(BLOCK_SIZE_DIM, BLOCK_SIZE_DIM);  
 
-	// Create events
-	cudaEvent_t event1, event2;
-	checkCuda(cudaEventCreate(&event1));
-	checkCuda(cudaEventCreate(&event2));
+    // Create events
+    cudaEvent_t event1, event2;
+    checkCuda(cudaEventCreate(&event1));
+    checkCuda(cudaEventCreate(&event2));
 
-	// Record events around kernel launch
-	checkCuda(cudaEventRecord(event1, 0)); 
-	matrixProduct<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, N);
-	checkCuda(cudaEventRecord(event2, 0));
+    // Record events around kernel launch
+    checkCuda(cudaEventRecord(event1, 0)); 
+    matrixProduct<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, N);
+    checkCuda(cudaEventRecord(event2, 0));
 
-	// Synchronize
-	checkCuda(cudaEventSynchronize(event1));
-	checkCuda(cudaEventSynchronize(event2)); // Wait for the event to be executed!
+    // Synchronize
+    checkCuda(cudaEventSynchronize(event1));
+    checkCuda(cudaEventSynchronize(event2)); // Wait for the event to be executed!
 
-	// Calculate compute time
-	float dt_ms;
-	checkCuda(cudaEventElapsedTime(&dt_ms, event1, event2));
-	printf("Compute time: %.1f ms \n", dt_ms);
+    // Calculate compute time
+    float dt_ms;
+    checkCuda(cudaEventElapsedTime(&dt_ms, event1, event2));
+    printf("Compute time: %.1f ms \n", dt_ms);
 
-	checkCuda(cudaDeviceSynchronize());
-	checkCuda(cudaGetLastError());
+    checkCuda(cudaDeviceSynchronize());
+    checkCuda(cudaGetLastError());
 
-	// Copy results from device to the host
-	checkCuda(cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost));
+    // Copy results from device to the host
+    checkCuda(cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost));
 
-	checkCuda(cudaFree(d_a));
-	checkCuda(cudaFree(d_b));
-	checkCuda(cudaFree(d_c));
+    checkCuda(cudaFree(d_a));
+    checkCuda(cudaFree(d_b));
+    checkCuda(cudaFree(d_c));
 
-	// showResults(h_a, h_b, h_c);
+    // showResults(h_a, h_b, h_c);
 
-	cudaDeviceReset();
-	return 0;
+    cudaDeviceReset();
+    return 0;
 }
--- mmpmpi.c
+++ mmpmpi.c
@@ -5,7 +5,7 @@
 /* Author:       Alberto Pou Quirós (Github: bertini36)                      */ 
 /* Description:  This program performs a matrix product (A * B = C)          */
 /*               distributing the computation between multiple nodes         */
-/*				 with MPI technology                                         */
+/*               with MPI technology                                         */
 /* Compilation:  mpicc -g -Wall -o mmpmpi mmpmpi.c                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -19,90 +19,90 @@
 struct timeval start_time, end_time;
 
 void initializeMatrices(double matrix_a[N][N], double matrix_b[N][N]) {
-	int i, j;
-	srand(time(NULL));
-	for (i=0; i<N; i++) {
-		for (j=0; j<N; j++) {
-			matrix_a[i][j] = rand();
-			matrix_b[i][j] = rand();
-		}
-	}
+    int i, j;
+    srand(time(NULL));
+    for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+            matrix_a[i][j] = rand();
+            matrix_b[i][j] = rand();
+        }
+    }
 }
 
 void showResults(double matrix_a[N][N], double matrix_b[N][N], double matrix_c[N][N]) {
-	int i, j;
-	printf("***** MATRIX A ***** \n");
-	for (i=0; i<N; i++) {
-		for (j=0; j<N; j++) {
-			(j % N == N-1) ? printf("%.1f \n", matrix_a[i][j]) : printf("%.1f,", matrix_a[i][j]);
-		}
-	}
-	printf("***** MATRIX B ***** \n");
-	for (i=0; i<N; i++) {
-		for (j=0; j<N; j++) {
-			(j % N == N-1) ? printf("%.1f \n", matrix_b[i][j]) : printf("%.1f,", matrix_b[i][j]);
-		}
-	}
-	printf("***** RESULT MATRIX ***** \n");
-	for (i=0; i<N; i++) {
-		for (j=0; j<N; j++) {
-			(j % N == N-1) ? printf("%.1f \n", matrix_c[i][j]) : printf("%.1f,", matrix_c[i][j]);
-		}
-	}
+    int i, j;
+    printf("***** MATRIX A ***** \n");
+    for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+            (j % N == N-1) ? printf("%.1f \n", matrix_a[i][j]) : printf("%.1f,", matrix_a[i][j]);
+        }
+    }
+    printf("***** MATRIX B ***** \n");
+    for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+            (j % N == N-1) ? printf("%.1f \n", matrix_b[i][j]) : printf("%.1f,", matrix_b[i][j]);
+        }
+    }
+    printf("***** RESULT MATRIX ***** \n");
+    for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+            (j % N == N-1) ? printf("%.1f \n", matrix_c[i][j]) : printf("%.1f,", matrix_c[i][j]);
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
 
-	double A[N][N], B[N][N], C[N][N];
-	int my_rank, comm_sz, from, to, i, j, k;
+    double A[N][N], B[N][N], C[N][N];
+    int my_rank, comm_sz, from, to, i, j, k;
   
-	// MPI initialization
-	MPI_Init (&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);    // Process id 
-	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);    // Number of processors 
+    // MPI initialization
+    MPI_Init (&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);    // Process id 
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);    // Number of processors 
 
-	if (N % comm_sz != 0) {
-		if (my_rank == 0) printf("Matrix size not divisible by number of processors \n");
-		MPI_Finalize();
-		exit(-1);
-	}
+    if (N % comm_sz != 0) {
+        if (my_rank == 0) printf("Matrix size not divisible by number of processors \n");
+        MPI_Finalize();
+        exit(-1);
+    }
 
-	// Calculate interval lines to compute per node
-	from = my_rank * N / comm_sz;
-	to = (my_rank + 1) * N / comm_sz;
+    // Calculate interval lines to compute per node
+    from = my_rank * N / comm_sz;
+    to = (my_rank + 1) * N / comm_sz;
 
-	if (my_rank == 0) {
-		srand(time(NULL));
-		initializeMatrices(A, B);
-	}
+    if (my_rank == 0) {
+        srand(time(NULL));
+        initializeMatrices(A, B);
+    }
 
-	// Send necessary information to every process
-	MPI_Bcast(B, N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	MPI_Scatter(A, N*N/comm_sz, MPI_DOUBLE, A[from], N*N/comm_sz, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    // Send necessary information to every process
+    MPI_Bcast(B, N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatter(A, N*N/comm_sz, MPI_DOUBLE, A[from], N*N/comm_sz, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	MPI_Barrier(MPI_COMM_WORLD);
-	if (my_rank == 0) { gettimeofday(&start_time, NULL); }
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (my_rank == 0) { gettimeofday(&start_time, NULL); }
 
-	// Calculate the product
-	for (i=from; i<to; i++) 
-		for (j=0; j<N; j++) {
-			C[i][j] = 0;
-		for (k=0; k<N; k++)
-			C[i][j] += A[i][k] * B[k][j];
-	}
+    // Calculate the product
+    for (i=from; i<to; i++) 
+        for (j=0; j<N; j++) {
+            C[i][j] = 0;
+        for (k=0; k<N; k++)
+            C[i][j] += A[i][k] * B[k][j];
+    }
 
-	// Calculate compute time
-	MPI_Barrier(MPI_COMM_WORLD);
-	if (my_rank == 0) { 
-		gettimeofday(&end_time, NULL);
-		printf("Compute time: %.1f ms \n", (float) (end_time.tv_sec - start_time.tv_sec) * 1000 + (end_time.tv_usec - start_time.tv_usec) / 1000);
-	 }
+    // Calculate compute time
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (my_rank == 0) { 
+        gettimeofday(&end_time, NULL);
+        printf("Compute time: %.1f ms \n", (float) (end_time.tv_sec - start_time.tv_sec) * 1000 + (end_time.tv_usec - start_time.tv_usec) / 1000);
+     }
 
-	// Receive results from every process
-	MPI_Gather(C[from], N*N/comm_sz, MPI_DOUBLE, C, N*N/comm_sz, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    // Receive results from every process
+    MPI_Gather(C[from], N*N/comm_sz, MPI_DOUBLE, C, N*N/comm_sz, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	// if (my_rank == 0) { showResults(A, B, C); }
+    // if (my_rank == 0) { showResults(A, B, C); }
 
-	MPI_Finalize();
-	return 0;
+    MPI_Finalize();
+    return 0;
 }
